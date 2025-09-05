f162312cf4e73c96b11df3d8610c479a3ab2f984
Talyslan <talyslancpc@gmail.com>
2025-09-05 01:41:07 -0300

    fix: removendo caminhos que nao existem mais

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
-import { fonts } from './src/config/fonts';
 
 // https://astro.build/config
 export default defineConfig({
     vite: {
         plugins: [tailwindcss()]
-    },
-    experimental: {
-        fonts: fonts
     }
 });
