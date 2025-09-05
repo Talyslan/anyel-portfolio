1bf9fe61a561dc0f8c26c82e32d5e389b9e616ca
Talyslan <talyslancpc@gmail.com>
2025-09-05 17:46:53 -0300

    feat: trocando imagem do dono do portfolio

--- src/components/index.ts
+++ src/components/index.ts
@@ -6,3 +6,5 @@ export { default as Footer } from './Footer.astro';
 export { default as Header } from './Header.astro';
 export { default as LandingPage } from './LandingPage.astro';
 export { default as BoobleBlur } from './BoobleBlur.astro';
+export { default as CustomRectangle } from './CustomRectangle.astro';
+
