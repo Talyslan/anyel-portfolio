d5348474066b1abfa7439bea13703683a07ef21b
Talyslan <talyslancpc@gmail.com>
2025-09-08 07:12:17 -0300

    feat: adicionando todos os boobles e icone de arrow, além de ajustar o nome de um dos clientes

--- src/assets/clients/index.ts
+++ src/assets/clients/index.ts
@@ -1,5 +1,5 @@
 export { default as DerickCarneiro } from './derick-carneiro.png';
 export { default as DialogandoMais } from './dialogando-mais.png';
-export { default as Leandro } from './leandro.png';
+export { default as LeandroRibeiro } from './leandro-ribeiro.png';
 export { default as LuanaLeite } from './luana-leite.png';
 export { default as TiagoFilemon } from './tiago-filemon.png';
